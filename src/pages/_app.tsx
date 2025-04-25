476c13f59dc430280f2095a9c79b19eb9630df4c
nahuel prado <nahuelprado16@gmail.com>
2025-04-25 09:34:13 -0300

    Carrito de compras

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,20 @@
 import type { AppProps } from 'next/app';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
+import { CartProvider } from '../components/ui/CartContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <CartProvider>
       <Header />
       <main className="min-h-screen">
         <Component {...pageProps} />
       </main>
       <Footer />
-    </>
+    </CartProvider>
   );
 }
+
 
 export default MyApp;
